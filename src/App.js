0ed432f5f0ad977db1386c0e76041387e1a411f3
Abilash <abilashb2000@gmail.com>
2023-11-02 13:36:48 +0530

    established connection between select button of friend and split bill form of friend

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ function Button({ children, onClick }) {
 function App() {
   const [friends, setFriends] = useState(initialFriends);
   const [showAddFriend, setShowAddFriend] = useState(false);
+  const [selectedFriend, setSelectedFriend] = useState(null);
 
   function handleShowAddFriend() {
     setShowAddFriend((show) => !show);
@@ -40,12 +41,22 @@ function App() {
 
   function handleAddFriend(newFriend) {
     setFriends((friends) => [...friends, newFriend]);
+    //hide the addfriend form after sumbitted
+    setShowAddFriend(false);
+  }
+
+  function handleSelection(friend) {
+    setSelectedFriend(friend);
   }
 
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList friends={friends} />
+        <FriendsList
+          friends={friends}
+          onHandleSelection={handleSelection}
+          selectedFriend={selectedFriend}
+        />
 
         {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
 
@@ -53,25 +64,38 @@ function App() {
           {showAddFriend ? "Close" : "Add Friend"}
         </Button>
       </div>
-      <FormSplitBill />
+
+      {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} />}
     </div>
   );
 }
 
-function FriendsList({ friends }) {
-  console.log(friends);
+function FriendsList({ friends, onHandleSelection, selectedFriend }) {
   return (
     <ul>
       {friends.map((friend) => (
-        <Friend friend={friend} key={friend.id} />
+        <Friend
+          friend={friend}
+          key={friend.id}
+          onHandleSelection={onHandleSelection}
+          selectedFriend={selectedFriend}
+        />
       ))}
     </ul>
   );
 }
 
-function Friend({ friend }) {
+function Friend({ friend, onHandleSelection, selectedFriend }) {
+  //initally no friend is selected so selectedFriend will be null, so accessing it causes error, so we use optional chaining
+  const isSelected = selectedFriend?.id === friend.id;
+
+  function handleSelection(friend) {
+    // console.log(friend);
+    onHandleSelection(friend);
+  }
+
   return (
-    <li>
+    <li className={isSelected ? "selected" : ""}>
       <img src={friend.image} alt={friend.name} />
       <h3>{friend.name}</h3>
 
@@ -87,8 +111,35 @@ function Friend({ friend }) {
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
 
-      <Button>Select</Button>
+      <Button onClick={() => handleSelection(friend)}>
+        {isSelected ? "close" : "select"}
+      </Button>
     </li>
+  );
+}
+
+function FormSplitBill({ selectedFriend }) {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with {selectedFriend.name}</h2>
+
+      <label>Bill Value</label>
+      <input type="text" />
+
+      <label>Your expense</label>
+      <input type="text" />
+
+      <label>{selectedFriend.name} 's expense</label>
+      <input type="text" disabled />
+
+      <label>Who is Paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">{selectedFriend.name}</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
   );
 }
 
@@ -102,7 +153,7 @@ function FormAddFriend({ onAddFriend }) {
     //guard when submitted empty
     if (!name || !image) return;
 
-    const id = date.now();
+    const id = Date.now();
     const newFriend = {
       id,
       name,
@@ -136,35 +187,9 @@ function FormAddFriend({ onAddFriend }) {
     </form>
   );
 }
-
-function FormSplitBill() {
-  return (
-    <form className="form-split-bill">
-      <h2>Split a bill with X</h2>
-
-      <label>Bill Value</label>
-      <input type="text" />
-
-      <label>Your expense</label>
-      <input type="text" />
-
-      <label>X 's expense</label>
-      <input type="text" disabled />
-
-      <label>Who is Paying the bill</label>
-      <select>
-        <option value="user">You</option>
-        <option value="friend">X</option>
-      </select>
-
-      <Button>Split bill</Button>
-    </form>
-  );
-}
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>
+  </>
 );
